114b63da75f4d7762bcbefb42209057b39bd359a
phucpham13123 <phucpham21109@gmail.com>
2024-11-04 00:33:14 +0700

    update layout job detail

--- src/candidate/components/job-details/job-content.tsx
+++ src/candidate/components/job-details/job-content.tsx
@@ -1,65 +1,83 @@
+import IconRedDot from '../../assets/svg/icon_Reddot.svg';
+
 const JobMainContent = () => {
+  const topReasons = [
+    "Working in a professional environment",
+    "Unlimited bonus according to work performance",
+    "Fully insured according to country regulations"
+  ];
+
+  const jobDescription = [
+    "Working in a professional environment",
+    "Unlimited bonus according to work performance",
+    "Fully insured according to country regulations"
+  ];
+
+  const skillsAndExperience = [
+    "Experience in ReactJS, NextJS, JavaScript",
+    "Familiar with front-end development",
+    "Good communication skills"
+  ];
+
+  const whyLoveWorkingHere = [
+    "Experience in ReactJS, NextJS, JavaScript",
+    "Familiar with front-end development",
+    "Good communication skills"
+  ];
+
   return (
-    <div className="p-5 bg-white rounded-lg shadow-md mb-5">
-      <section className="mb-5">
-        <h3 className="text-lg font-bold mb-2">Top 3 reasons to join us</h3>
-        <ul className="pl-5 mb-2 list-disc">
-          <li className="mb-1 text-base text-black">
-            Working in a professional environment
-          </li>
-          <li className="mb-1 text-base text-black">
-            Unlimited bonus according to work performance
-          </li>
-          <li className="mb-1 text-base text-black">
-            Fully insured according to country regulations
-          </li>
-        </ul>
-      </section>
+    <div className="p-5">
+      <div className="p-5 border bg-white rounded-lg shadow-md mb-5">
 
-      <section className="mb-5">
-        <h3 className="text-lg font-bold mb-2">Job description</h3>
-        <ul className="pl-5 mb-2 list-disc">
-          <li className="mb-1 text-base text-black">
-            Working in a professional environment
-          </li>
-          <li className="mb-1 text-base text-black">
-            Unlimited bonus according to work performance
-          </li>
-          <li className="mb-1 text-base text-black">
-            Fully insured according to country regulations
-          </li>
-        </ul>
-      </section>
+        <section className="mb-4 pb-3 border-dashed border-b">
+          <h3 className="text-lg font-bold mb-2">Top 3 reasons to join us</h3>
+          <ul className="pl-3 mb-2 list-disc">
+            {topReasons.map((reason, index) => (
+              <li key={index} className="mb-2 flex gap-2">
+                <img src={IconRedDot} alt="Red dot icon" />
+                {reason}
+              </li>
+            ))}
+          </ul>
+        </section>
 
-      <section className="mb-5">
-        <h3 className="text-lg font-bold mb-2">Your skills and experience</h3>
-        <ul className="pl-5 mb-2 list-disc">
-          <li className="mb-1 text-base text-black">
-            Experience in ReactJS, NextJS, JavaScript
-          </li>
-          <li className="mb-1 text-base text-black">
-            Familiar with front-end development
-          </li>
-          <li className="mb-1 text-base text-black">
-            Good communication skills
-          </li>
-        </ul>
-      </section>
+        <section className="mb-4 pb-3 border-dashed border-b">
+          <h3 className="text-lg font-bold mb-2">Job description</h3>
+          <ul className="pl-3 mb-2 list-disc">
+            {jobDescription.map((desc, index) => (
+              <li key={index} className="mb-2 flex gap-2">
+                <img src={IconRedDot} alt="Red dot icon" />
+                {desc}
+              </li>
+            ))}
+          </ul>
+        </section>
 
-      <section className="mb-5">
-        <h3 className="text-lg font-bold mb-2">Your skills and experience</h3>
-        <ul className="pl-5 mb-2 list-disc">
-          <li className="mb-1 text-base text-black">
-            Experience in ReactJS, NextJS, JavaScript
-          </li>
-          <li className="mb-1 text-base text-black">
-            Familiar with front-end development
-          </li>
-          <li className="mb-1 text-base text-black">
-            Good communication skills
-          </li>
-        </ul>
-      </section>
+        <section className="mb-4 pb-3 border-dashed border-b">
+          <h3 className="text-lg font-bold mb-2">Your skills and experience</h3>
+          <ul className="pl-3 mb-2 list-disc">
+            {skillsAndExperience.map((skill, index) => (
+              <li key={index} className="mb-2 flex gap-2">
+                <img src={IconRedDot} alt="Red dot icon" />
+                {skill}
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section>
+          <h3 className="text-lg font-bold mb-2">Why you'll love working here</h3>
+          <ul className="pl-3 mb-2 list-disc">
+            {whyLoveWorkingHere.map((reason, index) => (
+              <li key={index} className="mb-2 flex gap-2">
+                <img src={IconRedDot} alt="Red dot icon" />
+                {reason}
+              </li>
+            ))}
+          </ul>
+        </section>
+
+      </div>
     </div>
   );
 };
--- src/candidate/pages/job-details/job-details.tsx
+++ src/candidate/pages/job-details/job-details.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import JobHeader from "../../components/job-details/job-header";
 import JobMainContent from "../../components/job-details/job-content";
 import CompanySidebar from "../../components/job-details/company-sidebar";
+import JobListCompany from "../../components/job-details/job-listcompany";
 const JobDetail: React.FC = () => {
   return (
-    <div className="container-default md:flex">
-      <div className="md:w-2/3 p-4">
+    <div className="container-default md:flex bg-[#f6f6f6]">
+      <div className="md:w-2/3">
         <JobHeader />
         <JobMainContent />
+        <JobListCompany />
       </div>
-      <div className="md:w-1/3 p-4">
+      <div className="md:w-1/3 h-[435px] pr-5 py-10 bg-gradient-to-b from-[#78B6FF] to-[#3B95FF]"> 
         <CompanySidebar />
       </div>
     </div>
