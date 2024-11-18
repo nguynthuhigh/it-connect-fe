3f58c52cec10ef6de88fa81fc7cf56f22f0c38b8
phucpham13123 <phucpham21109@gmail.com>
2024-11-18 18:28:30 +0700

    fix absolute and reponsive

--- src/candidate/components/job-details/job-listcompany.tsx
+++ src/candidate/components/job-details/job-listcompany.tsx
@@ -1,9 +1,9 @@
 import FormJobListCompany from "./form-job-listcompany";
 
 const JobListCompany = () => {
-  
   return (
-    <div className='p-5 grid grid-cols-2 gap-5'>
+    <div className="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-5">
+      <FormJobListCompany />
       <FormJobListCompany />
       <FormJobListCompany />
       <FormJobListCompany />
