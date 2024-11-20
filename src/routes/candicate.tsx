b66b0761837b141b484c7b6c974cb080d0ded7c0
mint0000 <86461781+minhttt@users.noreply.github.com>
2024-11-20 12:33:43 +0700

    fix conflict

--- src/routes/candicate.tsx
+++ src/routes/candicate.tsx
@@ -1,6 +1,6 @@
 import CompanyDetails from "../candidate/pages/company/company-details";
 import Home from "../candidate/pages/home";
-import JobDetail from "../candidate/pages/job-details/job-details";
+import JobDetail from "../candidate/pages/job/job-details";
 import CompanyReviews from "../candidate/pages/company/company-review";
 import CompanyWriteReviews from "../candidate/pages/company/company-write-review";
 import Register from "../candidate/pages/authentication/register";
@@ -12,8 +12,6 @@ import { Outlet } from "react-router-dom";
 import FormVerify from "../candidate/pages/authentication/form-verify";
 import MyAccount from "../candidate/pages/profile/my-account";
 import CvManage from "../candidate/pages/profile/manage-cv";
-// import WriteReview from "../candidate/pages/company/write-review"
-// import Setting from "../candidate/pages/authentication/setting"
 import MyJobs from "../candidate/pages/profile/my-jobs";
 import JobInvitation from "../candidate/pages/job-invitation/job-invitation";
 import RegisterCompany from "../candidate/pages/register-company/register-company";
@@ -55,7 +53,7 @@ export const CandidateRoute = {
       element: <CompanyWriteReviews />,
     },
     {
-      path: "job",
+      path: "job/:slug",
       element: <JobDetail />,
     },
     {
